4685b78bd16f1843200142f63be1155bad748f30
Joseph Bennett <shadrxninga@gmail.com>
2011-06-11 13:01:57 +1200

    First Commit. V0.2

---
+++ src/com/herocraftonline/shadrxninga/herospawn/HeroSpawn.java
@@ -0,0 +1,114 @@
+package com.herocraftonline.shadrxninga.herospawn;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import org.bukkit.plugin.Plugin;
+
+public class HeroSpawn extends JavaPlugin{
+	static String mainDirectory = "plugins/HeroSpawn";
+	File file = new File(mainDirectory + File.separator + "spawnlocations.yml");
+	//Set up Listener Classes
+	private final HeroSpawnPlayerListener playerListener = new HeroSpawnPlayerListener(this);
+	
+	public static final Logger log = Logger.getLogger("Minecraft");
+	//Permissions Vairiables
+	public static PermissionHandler permissionHandler;
+
+	@Override
+	public void onDisable() {
+		log.info("[HeroSpawn] Version 0.2 Disabled");
+	}
+
+	@Override
+	public void onEnable() {
+		
+		new File(mainDirectory).mkdir();
+		if(!file.exists()){
+		            try {
+		                file.createNewFile();
+		            } catch (Exception ex) {
+		                ex.printStackTrace();
+		            }
+		        } else {
+		        	PluginManager pm = getServer().getPluginManager();
+		    		pm.registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Event.Priority.Highest, this);		
+		    		pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Event.Priority.Highest, this);		
+		    		log.info("[HeroSpawn] Version 0.2 Enabled");		        }
+		}
+	public void write(String root, Object x){     	
+		Configuration config = load();
+		config.setProperty(root, x);
+        config.save();
+    }
+
+    public String read(String root){
+    	Configuration config = load();
+        return config.getString(root);
+    }
+	public Configuration load(){
+
+        try {
+            Configuration config = new Configuration(file);
+            config.load();
+            return config;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+    	
+    	if (!(sender instanceof Player)) {
+    		sender.sendMessage(ChatColor.RED + "This command has to be called by a player!");
+    		return true;
+    	}
+    	Player player = (Player)sender;
+    	if (command.getName().equalsIgnoreCase("herospawn")) {
+    		if (!player.isOp()) {
+        		player.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+        		return true;
+    		}
+    		if(args.length==0){
+    			double z = Double.parseDouble(read(player.getWorld().getName() + ".Z"));
+    			double x = Double.parseDouble(read(player.getWorld().getName() + ".X"));
+    			double y = Double.parseDouble(read(player.getWorld().getName() + ".Y"));
+    			Location loc = new Location(player.getWorld(), x, y , z);
+    			player.teleport(loc);
+    			player.sendMessage("¤f[¤9HeroSpawn¤f]¤e Welcome to First Spawn");
+    			return true;
+	
+    		}
+    		if(args[0].equalsIgnoreCase("set")){
+        	    String x1 = Double.toString(player.getLocation().getX());
+        		String y1 = Double.toString(player.getLocation().getY());
+        		String z1 = Double.toString(player.getLocation().getZ());
+        		write(player.getWorld().getName() + ".X", x1);
+        		write(player.getWorld().getName() + ".Y", y1);
+        		write(player.getWorld().getName() + ".Z", z1);
+    			player.sendMessage(ChatColor.BLUE + "¤f[¤9HeroSpawn¤f] "  + ChatColor.YELLOW + "Players will now spawn here on their first login");
+    			
+        	} else{
+		return false;
+    	}
+		return true;
+	}
+		return false;
+}
+}
+
+
+
---
+++ src/com/herocraftonline/shadrxninga/herospawn/HeroSpawnPlayerListener.java
@@ -0,0 +1,54 @@
+package com.herocraftonline.shadrxninga.herospawn;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class HeroSpawnPlayerListener extends PlayerListener {
+	public static final Logger log = Logger.getLogger("Minecraft");
+    HashMap<String, Integer> login = new HashMap<String, Integer>();
+    private HeroSpawn plugin;
+
+	public HeroSpawnPlayerListener(HeroSpawn plugin) {
+        this.plugin = plugin;
+    }
+
+
+	
+	public void onPlayerLogin(PlayerLoginEvent e){
+		Player p = e.getPlayer();
+		String name = p.getName();
+		File file=new File(p.getWorld().getName() +"/players/" + name + ".dat");
+				  boolean exists = file.exists();
+				  if (!exists) {
+					  login.put(name, 1);
+				 System.out.println("[HeroSpawn] " + name + ": logged in for first time. Teleporting them to First Spawn");
+				  
+				  }else{
+					  
+					  if(login.containsKey(name))
+					  login.remove(name);
+				  }
+				  }
+		
+	
+	public void onPlayerJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		String name = p.getName();
+		if(login.containsKey(name)){
+		double z = Double.parseDouble(plugin.read(p.getWorld().getName() + ".Z"));
+		double x = Double.parseDouble(plugin.read(p.getWorld().getName() + ".X"));
+		double y = Double.parseDouble(plugin.read(p.getWorld().getName() + ".Y"));
+		Location loc = new Location(p.getWorld(), x, y , z);
+		p.teleport(loc);
+		}else{
+	}
+	}
+
+}
